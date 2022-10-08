7d7045c9f8e89cd79315b141724d8c2df2a19cf0
minh <minhnvgch200213@fpt.edu.vn>
2022-10-08 17:13:43 +0700

    final123

--- routes/student.js
+++ routes/student.js
@@ -18,4 +18,19 @@ router.get('/', (req, res) => {
 // router.get('/student', (req, res) => {
 //     res.send('test')
 // })
+
+
+///API
+
+
+router.get('/', (req, res) => {
+    StudentModel.find((err, data) => {
+        if (!err) {
+            //res.send(data)
+            //render ra trang index ở thư mục views/student
+            res.json(data)
+        }
+    })
+})
+
 module.exports = router
